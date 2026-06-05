42a0f0ddc8b5fb5a11ff8870dd470c989b7c21a5
Alex Haydock <alex@alexhaydock.co.uk>
2026-06-06 00:12:37 +0100

    Rework structure

--- scripts/10-build.sh
+++ scripts/10-build.sh
@@ -11,42 +11,30 @@ set -eoux pipefail
 
 # Source helper functions
 # shellcheck source=/dev/null
-source /ctx/build/copr-helpers.sh
+source /ctx/scripts/copr-helpers.sh
 
 # Enable nullglob for all glob operations to prevent failures on empty matches
 shopt -s nullglob
 
 echo "::group:: Copy Bluefin Config from Common"
 
-# Copy just files from @projectbluefin/common (includes 00-entry.just which imports 60-custom.just)
-mkdir -p /usr/share/ublue-os/just/
-shopt -s nullglob
-cp -r /ctx/oci/common/bluefin/usr/share/ublue-os/just/* /usr/share/ublue-os/just/
-shopt -u nullglob
+# See: https://github.com/projectbluefin/common/tree/main
+# TODO: Start migrating these files into this repo
+cp -rv /ctx/oci/common/shared/. /
+cp -rv /ctx/oci/common/bluefin/. /
 
 echo "::endgroup::"
 
 echo "::group:: Copy Custom Files"
 
-# Consolidate custom Justfiles
-find /ctx/custom/ujust -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >> /usr/share/ublue-os/just/60-custom.just
-
-# Copy Flatpak preinstall files
-mkdir -p /etc/flatpak/preinstall.d/
-cp /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/
-
-# Copy Bluefin common files
-# See: https://github.com/projectbluefin/common/tree/main
-# TODO: Start migrating these files into this repo
-cp -fvr /ctx/oci/common/shared/. /
-cp -fvr /ctx/oci/common/bluefin/. /
+cp -rv /ctx/root/. /
 
 echo "::endgroup::"
 
 echo "::group:: Install Packages"
 
 # Install packages using dnf5
-dnf5 install -y glow # Terminal-based Markdown reader, used by Bluefin motd
+dnf5 install -y glow # Terminal-based Markdown reader, used by Bluefin MOTD
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
@@ -63,10 +51,10 @@ systemctl enable podman.socket
 # See: https://github.com/projectbluefin/common/tree/main/system_files/bluefin/usr/lib/systemd
 systemctl enable flatpak-preinstall.service
 
-echo "::endgroup::"
-
 # Generate image-info.json for the MOTD to consume
-/ctx/build/00-image-info.sh
+/ctx/scripts/00-image-info.sh
+
+echo "::endgroup::"
 
 # Restore default glob behavior
 shopt -u nullglob
