4eaa58af99d90bccaf9617f80feab46886febe1f
Alex Haydock <alex@alexhaydock.co.uk>
2026-06-06 15:59:45 +0100

    Modularity

---
+++ scripts/20-packages.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+echo "::group:: ===$(basename "$0")==="
+
+set -ouex pipefail
+
+# Install Universal Blue update service
+copr_install_isolated "ublue-os/packages" "uupd"
+
+# Define package install set
+FEDORA_PACKAGES=(
+    fastfetch
+    glow
+)
+
+# Install all Fedora packages (bulk - safe from COPR injection)
+echo "Installing ${#FEDORA_PACKAGES[@]} packages from Fedora repos..."
+dnf -y install "${FEDORA_PACKAGES[@]}"
+
+# Packages to exclude - common to all versions
+EXCLUDED_PACKAGES=(
+    cosign
+    fedora-bookmarks
+    fedora-chromium-config
+    fedora-chromium-config-gnome
+    firefox
+    firefox-langpacks
+    gnome-extensions-app
+    gnome-shell-extension-background-logo
+    gnome-software
+    gnome-software-rpm-ostree
+    gnome-terminal-nautilus
+    podman-docker
+    yelp
+)
+
+# Remove excluded packages if they are installed
+if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    readarray -t INSTALLED_EXCLUDED < <(rpm -qa --queryformat='%{NAME}\n' "${EXCLUDED_PACKAGES[@]}" 2>/dev/null || true)
+    if [[ "${#INSTALLED_EXCLUDED[@]}" -gt 0 ]]; then
+        dnf -y remove "${INSTALLED_EXCLUDED[@]}"
+    else
+        echo "No excluded packages found to remove."
+    fi
+fi
+
+echo "::endgroup::"
