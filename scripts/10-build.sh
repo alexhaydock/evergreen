c332b1781792f32e56ceeb3312698a4210915fa3
Alex Haydock <alex@alexhaydock.co.uk>
2026-06-06 15:44:34 +0100

    Fixes from upstream

--- scripts/10-build.sh
+++ scripts/10-build.sh
@@ -49,12 +49,15 @@ systemctl enable podman.socket
 # See: https://github.com/ublue-os/aurorafin-shared/tree/main/system_files/shared/usr/lib/systemd
 # See: https://github.com/projectbluefin/common/tree/main/system_files/bluefin/usr/lib/systemd
 systemctl --global enable podman-auto-update.timer
-systemctl --global enable ublue-user-setup.service
 systemctl enable dconf-update.service
 systemctl enable flatpak-preinstall.service
-systemctl enable input-remapper.service
-systemctl enable ublue-system-setup.service
+
+# Universal Blue updater
 systemctl enable uupd.timer
+
+# Universal Blue setup services
+systemctl --global enable ublue-user-setup.service
+systemctl enable ublue-system-setup.service
 
 # Disable the old rpm-ostreed-automatic.timer
 systemctl disable rpm-ostreed-automatic.timer
