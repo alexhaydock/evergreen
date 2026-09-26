6737b42fa2658857ab161f689093f9e556c682d4
Alex Haydock <alex@alexhaydock.co.uk>
2026-09-26 21:13:13 +0100

    Remove duplicated cleanup code

--- scripts/99-cleanup.sh
+++ scripts/99-cleanup.sh
@@ -130,9 +130,6 @@ rm -rf /tmp/* || true
 rm -rf /usr/etc
 # shellcheck disable=SC2114
 rm -rf /boot && mkdir /boot
-# Preserve cache mounts
-find /var/* -maxdepth 0 -type d \! -name cache \! -name log -exec rm -rf {} \;
-find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 -exec rm -rf {} \;
 
 # Make sure /var/tmp is properly created
 mkdir -p /var/tmp
